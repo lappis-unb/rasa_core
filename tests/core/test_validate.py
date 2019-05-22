b5c65159214fe5b81b7c2f5ee50cf6800d72bb56
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-05-22 11:46:13 -0300

    Change tests
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- tests/core/test_validate.py
+++ tests/core/test_validate.py
@@ -1,34 +1,40 @@
+import pytest
 import asyncio
 from rasa.core.validate import Validate
+from tests.core.conftest import DEFAULT_DOMAIN_PATH, DEFAULT_STORIES_FILE, DEFAULT_NLU_DATA
+from rasa.core.domain import Domain
+from rasa_nlu.training_data import load_data, TrainingData
+from rasa.core.training.dsl import StoryFileReader
 
-domain_file = "data/test_validate/domain.yml"
-intents_file = "data/test_validate/intents.md"
-stories_file = "data/test_validate/stories.md"
+@pytest.fixture
+def validate():
+    domain = Domain.load(DEFAULT_DOMAIN_PATH)
+    stories = asyncio.run(
+        StoryFileReader.read_from_folder(DEFAULT_STORIES_FILE, domain)
+    )
+    intents = load_data(DEFAULT_NLU_DATA)
 
-validate_test = Validate(domain=domain_file, intents=intents_file, stories=stories_file)
-
-
-def test_validate_creation():
-
-    assert isinstance(validate_test.domain, Domain)
-    assert isinstance(validate_test.intents, TrainingData)
-    assert isinstance(validate_test.stories, list)
-
-
-def test_search():
-    vec = ["a", "b", "c", "d", "e"]
-    assert validate_test._search(vector=vec, searched_value="c")
+    return Validate(domain=domain, intents=intents, stories=stories)
 
 
-def test_verify_intents():
-    valid_intents = ["greet", "goodbye", "affirm", "deny"]
+def test_validate_creation(validate):
+    assert isinstance(validate.domain, Domain)
+    assert isinstance(validate.intents, TrainingData)
+    assert isinstance(validate.stories, list)
 
-    validate_test.verify_intents()
-    assert validate_test.valid_intents == valid_intents
+
+def test_search(validate):
+    vec = ['a', 'b', 'c', 'd', 'e']
+    assert validate._search(vector=vec, searched_value='c')
 
 
-def test_verify_utters():
-    valid_utters = ["utter_greet", "utter_goodbye", "utter_question"]
+def test_verify_intents(validate):
+    valid_intents = ['greet', 'goodbye']
+    validate.verify_intents()
+    assert validate.valid_intents == valid_intents
 
-    validate_test.verify_utters()
-    assert validate_test.valid_utters == valid_utters
+
+def test_verify_utters(validate):
+    valid_utterances = ['utter_greet', 'utter_goodbye', 'utter_default']
+    validate.verify_utterances()
+    assert validate.valid_utterances == valid_utterances
